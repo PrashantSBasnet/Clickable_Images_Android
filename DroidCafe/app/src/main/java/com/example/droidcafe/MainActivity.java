0d722571bf8a94bc60ff9afa3021bda867cdd039
Prashant <prasbas97@gmail.com>
2021-03-07 16:03:37 +0545

    Challenging Exercise finished

--- DroidCafe/app/src/main/java/com/example/droidcafe/MainActivity.java
+++ DroidCafe/app/src/main/java/com/example/droidcafe/MainActivity.java
@@ -9,36 +9,45 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_MESSAGE =
+            "com.example.android.droidcafe.extra.MESSAGE";
+    public String mOrderMessage;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
-
     }
+
         public void displayToast(String message) {
             Toast.makeText(getApplicationContext(), message,
                     Toast.LENGTH_SHORT).show();
     }
 
 
+    //when donut image clicked
     public void showDonutOrder(View view) {
-        displayToast(getString(R.string.donut_order_message));
+        mOrderMessage = getString(R.string.donut_order_message);
+        displayToast(mOrderMessage);
     }
 
-
+    //when ice cream image clicked
     public void showIceCreamOrder(View view) {
-        displayToast(getString(R.string.ice_cream_order_message));
+        mOrderMessage = getString(R.string.ice_cream_order_message);
+        displayToast(mOrderMessage);
     }
 
-
+    //when froyo image clicked
     public void showFroyoOrder(View view) {
-        displayToast(getString(R.string.froyo_order_message));
+        mOrderMessage = getString(R.string.froyo_order_message);
+        displayToast(mOrderMessage);
     }
 
-
-
+    //sends order to OrderActivity
     public void onClick(View view) {
         Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
         startActivity(intent);
     }
 }
--- DroidCafe/app/src/main/java/com/example/droidcafe/OrderActivity.java
+++ DroidCafe/app/src/main/java/com/example/droidcafe/OrderActivity.java
@@ -2,7 +2,9 @@ package com.example.droidcafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class OrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+
+        //receives data
+        Intent intent = getIntent();
+        //to display received message in the textfield
+        String message = "Order: " + intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.order_textview);
+        textView.setText(message);
     }
 }
